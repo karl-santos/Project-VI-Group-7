5e0945f04024b256181dc460edb3033951346861
Karl Santos <ksantos7146@conestogac.on.ca>
2026-02-03 23:04:34 -0500

    comment service added

---
+++ Speedrun/Speedrun/Services/ICommentService.cs
@@ -0,0 +1,17 @@
+﻿using Speedrun.Models;
+
+namespace Speedrun.Services
+{
+
+    // Interface for comment management operations
+    public interface ICommentService
+    {
+        
+        // Gets all comments for a specific run
+        // Comments are sorted newest first
+        List<Comment> GetCommentsByRun(int runId);
+       
+        // Creates a new comment on a rum
+        Comment CreateComment(int runId, string username, string content);
+    }
+}
