cf9839f0207d1e82a591e0b728557b2e9d17894f
Karl Santos <ksantos7146@conestogac.on.ca>
2026-02-15 11:40:02 -0500

    removing unused files

--- Speedrun/Speedrun/DTOs/GameDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Speedrun.DTOs
-{
-    public class GameDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public string GameImageUrl { get; set; } = string.Empty;
-    }
-}
